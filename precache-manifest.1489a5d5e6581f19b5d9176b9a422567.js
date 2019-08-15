7d0108a2fb65bba689df0ee989f278496a584e20
GeorgPandeh <georgpandeh@gmail.com>
2019-08-15 23:32:55 +0300

    Updates

---
+++ precache-manifest.1489a5d5e6581f19b5d9176b9a422567.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d07311974e5eba42c29a8bff308ebe",
+    "url": "/piskel-clone/index.html"
+  },
+  {
+    "revision": "43b9518e44fdbcca8e24",
+    "url": "/piskel-clone/static/css/main.8ab1c383.chunk.css"
+  },
+  {
+    "revision": "1288eadbffc332ead585",
+    "url": "/piskel-clone/static/js/2.66c66ef7.chunk.js"
+  },
+  {
+    "revision": "43b9518e44fdbcca8e24",
+    "url": "/piskel-clone/static/js/main.3d72cd70.chunk.js"
+  },
+  {
+    "revision": "5654ab04200be8a1ca6b",
+    "url": "/piskel-clone/static/js/runtime~main.dff1cf59.js"
+  }
+]);
